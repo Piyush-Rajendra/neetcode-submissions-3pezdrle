567c7f6c4299cdbfc1d1afea67bbc95ebc3e473b
Piyush Rajendra <piyush.rajendra@gmail.com>
2026-04-09 12:35:50 -0400

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+
+        for char in operations:
+            if char == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif char == 'C':
+                stack.pop()
+            elif char == 'D':
+                stack.append( 2 * stack[-1])
+            else:
+                stack.append(int(char))
+        return sum(stack)
+        
+        
+        
+        
